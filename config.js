3501a6b2b3cbf12e8924210aeaf93312bb4239ea
Shivraj Chavan <shivrajchavan1902@gmail.com>
2024-07-21 11:16:09 +0530

    mongo url update

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 const getConnection=async()=>{
 
     try {
-        mongoose.connect(process.env.MONGO_URL)
+        mongoose.connect('mongodb+srv://shivrajchavan1902:WQgwA7BQFWyJA6KG@cluster0.1edqz2q.mongodb.net/')
         console.log('MongoDB Connected...');
     } catch (error) {
      console.log(`Error: ${error}`);
