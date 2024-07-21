65cf645ee18cfe996abcb100c4ec80a5249a844f
Shivraj Chavan <shivrajchavan1902@gmail.com>
2024-07-21 11:23:47 +0530

    a

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const sslOptions = {
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(301, { 'Location': `https://${req.headers['host']}${req.url}` });
+  console.log(`Location:https://${req.headers['host']}${req.url}`);
   res.end();
 }).listen(80);
 
